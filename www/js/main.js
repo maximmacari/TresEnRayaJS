9acd833bf600014944a8fa9b20da0aeaaf7b6eb8
gabrielrevilla <gabriel.revilla99@gmail.com>
2019-10-30 18:42:05 +0100

    CAMBIOS GABI - Miércoles 30/10

--- www/js/main.js
+++ www/js/main.js
@@ -7,6 +7,7 @@ class Main{
       maxScore: 0,
       score: 0
     };
+    this.sala = null;
   }
 
   logIn(){
@@ -24,8 +25,6 @@ class Main{
         this.jugador.username = resp.username;
         this.jugador.maxScore = resp.maxScore;
         this.jugador.hash = resp.hash;
-
-        juego.init();
       }else{
         tools.showModal("Iniciar sesión", "Usuario o contraseña incorrectos");
       }
@@ -36,5 +35,78 @@ class Main{
 
   signUp(){
 
+  }
+
+  crearSala(nombreSala, claveSala){
+    let dataSend = {
+      jugador: this.jugador,
+      nombreSala: nombreSala,
+      claveSala: claveSala
+    };
+
+    tools.httpPost("/crearSala", JSON.stringify(dataSend), function(msg){
+      let resp = JSON.parse(msg);
+      
+      alert(`State: ${resp.result} | MSG: ${resp.msg}`);
+    }, function(err){
+      console.error(err);
+    });
+  }
+
+  entrarSala(nombreSala, claveSala){
+    if(this.sala === null){
+      let dataSend = {
+        jugador: this.jugador,
+        nombreSala: nombreSala,
+        claveSala: claveSala
+      };
+
+      tools.httpPost("/entrarSala", JSON.stringify(dataSend), function(msg){
+        let resp = JSON.parse(msg);
+
+        if(resp.result){
+          main.sala = nombreSala;
+
+          juego.init();
+
+          tools.showModal("Entrar en sala", resp.msg);
+        }else{
+          tools.showModal("Entrar en sala", resp.msg);
+        }
+      }, function(err){
+        console.error(err);
+      });
+    }else{
+      tools.showModal("Info", "Ya estás en una sala!");
+    }
+  }
+
+  listarSalas(){
+    tools.httpGet("/listSalas", function(msg){
+      let resp = JSON.parse(msg);
+
+      let tableSalas = document.getElementById("tableSalas")
+        .getElementsByTagName("tbody")[0];
+
+      tableSalas.innerHTML = "";
+
+      for(let sala of resp){
+        let htmlSala = `
+        <td class="text-center">
+          ${sala.nombre}
+        </td>
+        <td class="text-center">
+          <button class="btn btn-primary btn-block">Entrar</button>
+        </td>
+        `.trim();
+
+        let tr = document.createElement("tr");
+        tr.innerHTML = htmlSala;
+
+        tableSalas.appendChild(tr);
+      }
+    }, function(err){
+      console.error(err);
+    });
   }
 }
