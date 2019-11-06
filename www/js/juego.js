8469c43b4f33f7b4f0d47555a6cf0b9a7f4a3f8e
gabrielrevilla <gabriel.revilla99@gmail.com>
2019-11-06 20:26:07 +0100

    GABI 06/11

--- www/js/juego.js
+++ www/js/juego.js
@@ -1,5 +1,5 @@
-class Juego{
-  constructor(){
+class Juego {
+  constructor() {
     this.offsetX = 0;
     this.offsetY = 0;
 
@@ -12,10 +12,10 @@ class Juego{
     this.casillas = [];
   }
 
-  celdaClick(position){
+  celdaClick(position) {
     let dataSend = {
       type: "celdaClick",
-      data:{
+      data: {
         nombreSala: main.sala,
         position: position,
         jugadorHash: main.jugadorHash
@@ -25,15 +25,15 @@ class Juego{
     this.ws.send(JSON.stringify(dataSend));
   }
 
-  generarTablero(sala){
+  generarTablero(sala) {
     let nCasillas = [3, 3];
-  
+
     this.casillas = [];
-  
-    for(let i=0;i<nCasillas[1];i++){
+
+    for (let i = 0; i < nCasillas[1]; i++) {
       let filaCasillas = [];
-  
-      for(let k=0;k<nCasillas[0];k++){
+
+      for (let k = 0; k < nCasillas[0]; k++) {
         let casilla = {
           position: {
             x: k,
@@ -41,18 +41,19 @@ class Juego{
           },
           value: "none"
         };
-  
+
         filaCasillas.push(casilla);
       }
-  
+
       sala.tablero.push(filaCasillas);
     }
   }
 
-  //Borra la tabla entera y la vuelve a pintar
-  pintarCasillas(){
-    this.tablero.innerHTML = "";
-    
+  actualizarCasillas() {
+    console.log("ACTUALIZANDO CASILLAS");
+
+    let rows = this.tablero.getElementsByTagName("tr");
+
     let svgCruz = `
     <svg aria-label="X" class="cruz" role="img" viewBox="0 0 128 128">
       <path class="casilla-cruz" d="M16,16L112,112"></path>
@@ -65,46 +66,79 @@ class Juego{
     </svg>
     `.trim();
 
-    for(let fila of this.casillas){
-      let row = document.createElement("tr");
-      
-      for(let celda of fila){
-        let htmlCasilla = `
-        <div class="casilla">
-        </div>
-        `;
-        let celdaCasilla = document.createElement("td");
-        celdaCasilla.innerHTML = htmlCasilla;
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i];
+      let cols = row.getElementsByTagName("td");
 
-        if(celda.value === "circulo"){
-          celdaCasilla.getElementsByClassName("casilla")[0].innerHTML = svgCirculo;
-        }else if(celda.value === "cruz"){
-          celdaCasilla.getElementsByClassName("casilla")[0].innerHTML = svgCruz;
-        }else{
+      for (let k = 0; k < cols.length; k++) {
+        let col = cols[k];
+        let casillaValue = juego.casillas[i][k].value;
 
+        console.log(casillaValue);
+
+        if (casillaValue === "circulo") {
+          col.getElementsByClassName("casilla")[0].innerHTML = svgCirculo;
+        } else if (casillaValue === "cruz") {
+          col.getElementsByClassName("casilla")[0].innerHTML = svgCruz;
+        } else {
+          col.getElementsByClassName("casilla")[0].innerHTML = "";
         }
-
-        celdaCasilla.addEventListener("click", () => {
-          juego.celdaClick(celda.position);
-        });
-
-        row.appendChild(celdaCasilla);
       }
-
-      this.tablero.appendChild(row);
     }
   }
 
-	//crear celdas
-  generarCasillas(){
+  //Borra la tabla entera y la vuelve a pintar
+  pintarCasillas() {
+    if (this.tablero.childElementCount <= 0) {
+      console.log("Pintando");
+      this.tablero.innerHTML = "";
+
+      let svgCruz = `
+      <svg aria-label="X" class="cruz" role="img" viewBox="0 0 128 128">
+        <path class="casilla-cruz" d="M16,16L112,112"></path>
+        <path class="casilla-cruz" d="M112,16L16,112"></path>
+      </svg>
+      `.trim();
+      let svgCirculo = `
+      <svg aria-label="O" class="circulo" role="img" viewBox="0 0 128 128">
+        <path class="casilla-circulo" d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16"></path>
+      </svg>
+      `.trim();
+
+      for (let fila of juego.casillas) {
+        let row = document.createElement("tr");
+
+        for (let celda of fila) {
+          let htmlCasilla = `
+          <div class="casilla">
+          </div>
+          `;
+          let celdaCasilla = document.createElement("td");
+          celdaCasilla.innerHTML = htmlCasilla;
+
+          celdaCasilla.addEventListener("click", () => {
+            juego.celdaClick(celda.position);
+          });
+
+          row.appendChild(celdaCasilla);
+        }
+
+        this.tablero.appendChild(row);
+      }
+    } else {
+      juego.actualizarCasillas();
+    }
+  }
+
+  //crear celdas
+  generarCasillas() {
     let nCasillas = [3, 3];
+    let casillas = [];
 
-    this.casillas = [];
-
-    for(let i=0;i<nCasillas[1];i++){
+    for (let i = 0; i < nCasillas[1]; i++) {
       let filaCasillas = [];
 
-      for(let k=0;k<nCasillas[0];k++){
+      for (let k = 0; k < nCasillas[0]; k++) {
         let casilla = {
           position: {
             x: k,
@@ -116,11 +150,13 @@ class Juego{
         filaCasillas.push(casilla);
       }
 
-      this.casillas.push(filaCasillas);
+      casillas.push(filaCasillas);
     }
+
+    return casillas;
   }
 
-  requestTablero(){
+  requestTablero() {
     let dataSend = {
       type: "getTablero",
       data: {
@@ -131,12 +167,12 @@ class Juego{
     juego.ws.send(JSON.stringify(dataSend));
   }
 
-  init(){
+  init() {
     main.displayJuego();
 
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-    this.ws = new WebSocket('ws://localhost:4741');
+    this.ws = new WebSocket('ws://192.168.3.148:4741');
 
     this.ws.onopen = function () {
       let dataSend = {
@@ -148,12 +184,12 @@ class Juego{
       juego.ws.send(JSON.stringify(dataSend));
 
       juego.requestTablero();
-      juego.intervalUpdate = setInterval(function(){
+      juego.intervalUpdate = setInterval(function () {
         juego.requestTablero();
       }, 10000);
     };
 
-    this.ws.onclose = function(){
+    this.ws.onclose = function () {
       clearInterval(juego.intervalUpdate);
       console.log("Conexión cerrada");
     }
@@ -167,8 +203,8 @@ class Juego{
       try {
         let msg = JSON.parse(message.data);
 
-        if(msg.type === "clients"){
-          for(let client of msg.data){
+        if (msg.type === "clients") {
+          for (let client of msg.data) {
             let root = document.createElement("div");
             let html = `
               <li>
@@ -178,15 +214,15 @@ class Juego{
             root.innerHTML = html;
 
             juego.usuariosList.getElementsByTagName("ul")[0]
-            .appendChild(root.firstChild);
+              .appendChild(root.firstChild);
           }
-        }else if(msg.type === "tablero"){
+        } else if (msg.type === "tablero") {
           juego.casillas = msg.data.tablero;
-          juego.pintarCasillas();
-        }else if(msg.type === "registrarJugador"){
-          if(msg.data.result){
-            console.log("JUGADOR REGISTRADO");  
-          }else{
+          juego.pintarCasillas(juego.generarCasillas());
+        } else if (msg.type === "registrarJugador") {
+          if (msg.data.result) {
+            console.log("JUGADOR REGISTRADO");
+          } else {
             console.log("ERROR AL REGISTRAR JUGADOR");
           }
         }
